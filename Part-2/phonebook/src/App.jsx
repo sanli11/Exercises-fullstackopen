53ca79ba1f6d8da4f35eb9f62aa30bc59338949d
SanLi <fate.sinz1@gmail.com>
2023-12-03 18:56:15 -0800

    Cleaned Code

--- Part-2/phonebook/src/App.jsx
+++ Part-2/phonebook/src/App.jsx
@@ -5,10 +5,17 @@ import Persons from "./components/Persons";
 import personService from "./services/axiosPerson";
 
 const App = () => {
+  /*
+      States
+  */
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [search, setSearch] = useState("");
+
+  /*
+      Variables
+  */
 
   // Using this to fix issue when (nth - 1) person is deleted
   // Afterwards if a new person is added, it will have same ID as the nth person
@@ -19,6 +26,9 @@ const App = () => {
       persons[persons.length - 1]
     ).id + 1;
 
+  /*
+      Effects
+  */
   useEffect(() => {
     console.log("Effect is now running");
 
@@ -28,6 +38,10 @@ const App = () => {
     });
   }, []);
 
+  /*
+      Functions
+  */
+
   // This snippet was taken from React Official Documentation
   // https://react.dev/learn/sharing-state-between-components#challenges
   let numbers = persons.filter((person) =>
@@ -36,37 +50,52 @@ const App = () => {
       .some((name) => name.toLowerCase().startsWith(search.toLowerCase()))
   );
 
+  /*
+    Input Event Handler Functions
+  */
   const handleName = (e) => setNewName(e.target.value);
   const handleNumber = (e) => setNewNumber(e.target.value);
   const handleSearch = (e) => setSearch(e.target.value);
 
+  /*
+    Button Event Handler Functions
+  */
+  const deletePerson = (id) => {
+    let deletedPerson = persons.find((person) => person.id === id);
+
+    if (window.confirm(`Delete ${deletedPerson.name} ?`)) {
+      personService
+        .deleteAPerson(id)
+        .then(() => setPersons(persons.filter((person) => person.id !== id)));
+    }
+  };
+
   const addPerson = (e) => {
     e.preventDefault();
 
+    /*
+      When name is same as an existing person
+    */
     if (persons.some((person) => person.name === newName)) {
       let matchedPerson = persons.find((person) => person.name === newName);
 
+      /*
+        When number is also same as the existing person
+      */
       if (matchedPerson.number === newNumber) {
-        console.log("Same name and number already in the phonebook");
-
         alert(`${newName} is already added in the phonebook`);
         return;
       }
 
-      console.log("Same name but different number in the phonebook");
-      if (
-        window.confirm(
-          `${newName} is already added in the phonebook. Do you want to replace the old number with the new one?`
-        )
-      ) {
-        let updatedPerson = {
-          name: newName,
-          number: newNumber,
-          id: matchedPerson.id,
-        };
+      /*
+        When number is different from the existing person
+      */
+      let windowText = `${newName} is already added in the phonebook. Do you want to replace the old number with the new one?`;
+      if (window.confirm(windowText)) {
+        matchedPerson = { ...matchedPerson, number: newNumber };
 
         personService
-          .updateAPerson(matchedPerson.id, updatedPerson)
+          .updateAPerson(matchedPerson.id, matchedPerson)
           .then((updatedPerson) =>
             setPersons(
               persons.map((person) =>
@@ -74,11 +103,15 @@ const App = () => {
               )
             )
           );
-          console.log("Person successfully updated");
+
+        console.log("Person successfully updated");
       } else {
         console.log("Update cancelled");
       }
     } else {
+    /*
+      When name is different from an existing person
+    */
       let newPerson = {
         name: newName,
         number: newNumber,
@@ -94,16 +127,6 @@ const App = () => {
         setPersons(persons.concat(newPersonAdded));
         clearInputFields();
       });
-    }
-  };
-
-  const deletePerson = (id) => {
-    let deletedPerson = persons.find((person) => person.id === id);
-
-    if (window.confirm(`Delete ${deletedPerson.name} ?`)) {
-      personService
-        .deleteAPerson(id)
-        .then(() => setPersons(persons.filter((person) => person.id !== id)));
     }
   };
 
