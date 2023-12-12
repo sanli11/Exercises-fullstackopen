44b13a53423af0150e98c8326125c11624ced7f7
SanLi <fate.sinz1@gmail.com>
2023-12-11 16:13:57 -0800

    Reformatted the code To move the commented Pre-Mongo code in single block

--- Part-3/phonebook-backend/index.js
+++ Part-3/phonebook-backend/index.js
@@ -11,10 +11,6 @@ morgan.token("data", function (req, res) {
   return JSON.stringify(req.body);
 });
 
-const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: "unknown endpoint" });
-};
-
 const Person = require("./models/person");
 
 const app = express();
@@ -31,43 +27,77 @@ app.use(
   )
 );
 
-// let persons = [
-//   {
-//     id: 1,
-//     name: "Arto Hellas",
-//     number: "040-123456",
-//   },
-//   {
-//     id: 2,
-//     name: "Ada Lovelace",
-//     number: "39-44-5323523",
-//   },
-//   {
-//     id: 3,
-//     name: "Dan Abramov",
-//     number: "12-43-234345",
-//   },
-//   {
-//     id: 4,
-//     name: "Mary Poppendieck",
-//     number: "39-23-6423122",
-//   },
-// ];
+/*
+let persons = [
+  { id: 1, name: "Arto Hellas", number: "040-123456", },
+  { id: 2, name: "Ada Lovelace", number: "39-44-5323523", },
+  { id: 3, name: "Dan Abramov", number: "12-43-234345", },
+  { id: 4, name: "Mary Poppendieck", number: "39-23-6423122", },
+];
 
-// const generateId = () => {
-//   let maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
+const generateId = () => {
+  let maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
+  return maxId + 1;
+};
 
-//   return maxId + 1;
-// };
+app.get("/info", (req, res) => {
+  console.log("Showing information about the phonebook");
+  let date = new Date();
+  // Using string literals to display on multiple lines
+  let content = `<p>Phonebook has info for ${persons.length} people</p>
+  <p>${date}</p>`;
+  res.send(content);
+});
 
-// app.get("/info", (req, res) => {
-//   console.log("Showing information about the phonebook");
-//   let date = new Date();
-//   // Using string literals to display on multiple lines
-//   let content = `<p>Phonebook has info for ${persons.length} people</p>
-//   <p>${date}</p>`;
-//   res.send(content);
-// });
+app.get("/api/persons", (req, res) => {
+  console.log("Showing all persons");
+  res.send(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  let id = Number(req.params.id);
+  let person = persons.find((p) => p.id === id);
+  if (person) {
+    console.log(`Showing person with id ${id}:`, person);
+    res.json(person);
+  } else {
+    console.log(`Person with id ${id} not found`);
+    res.status(404).send(`Person with id ${id} not found`);
+  }
+});
+
+app.post("/api/persons", (req, res) => {
+  const personInfo = req.body;
+  if (!personInfo.name) {
+    console.log("Name for person is required");
+    return res.status(400).json({ error: "Name for person is required" });
+  } else if (!personInfo.number) {
+    console.log("Number for person is required");
+    return res.status(400).json({ error: "Number for person is required" });
+  } else if (persons.find((p) => p.name === personInfo.name)) {
+    console.log(
+      "A person with the name already exists in the phonebook. Each name must be unique"
+    );
+    return res.status(400).json({ error: "name must be unique" });
+  }
+  const newPerson = {
+    id: generateId(),
+    name: personInfo.name,
+    number: personInfo.number,
+  };
+  persons = persons.concat(newPerson);
+  res.json(newPerson);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  console.log("Deleting person with ID: ", id);
+  persons = persons.filter((p) => p.id !== id);
+  console.log("Person deleted with ID: ", id);
+  res.status(204).end();
+});
+*/
+
 app.get("/info", (req, res) => {
   Person.countDocuments({}).then((count) => {
     let docs = count;
@@ -79,51 +109,14 @@ app.get("/info", (req, res) => {
   });
 });
 
-// app.get("/api/persons", (req, res) => {
-//   console.log("Showing all persons");
-//   res.send(persons);
-// });
 app.get("/api/persons", (req, res) => {
   Person.find({}).then((p) => res.json(p));
 });
 
-// app.get("/api/persons/:id", (req, res) => {
-//   let id = Number(req.params.id);
-//   let person = persons.find((p) => p.id === id);
-//   if (person) {
-//     console.log(`Showing person with id ${id}:`, person);
-//     res.json(person);
-//   } else {
-//     console.log(`Person with id ${id} not found`);
-//     res.status(404).send(`Person with id ${id} not found`);
-//   }
-// });
 app.get("/api/persons/:id", (req, res) => {
   Person.findById(req.params.id).then((p) => res.json(p));
 });
 
-// app.post("/api/persons", (req, res) => {
-//   const personInfo = req.body;
-//   if (!personInfo.name) {
-//     console.log("Name for person is required");
-//     return res.status(400).json({ error: "Name for person is required" });
-//   } else if (!personInfo.number) {
-//     console.log("Number for person is required");
-//     return res.status(400).json({ error: "Number for person is required" });
-//   } else if (persons.find((p) => p.name === personInfo.name)) {
-//     console.log(
-//       "A person with the name already exists in the phonebook. Each name must be unique"
-//     );
-//     return res.status(400).json({ error: "name must be unique" });
-//   }
-//   const newPerson = {
-//     id: generateId(),
-//     name: personInfo.name,
-//     number: personInfo.number,
-//   };
-//   persons = persons.concat(newPerson);
-//   res.json(newPerson);
-// });
 app.post("/api/persons", (req, res) => {
   const personInfo = req.body;
 
@@ -143,21 +136,18 @@ app.post("/api/persons", (req, res) => {
   person.save().then((savedPerson) => res.json(savedPerson));
 });
 
-// app.delete("/api/persons/:id", (req, res) => {
-//   const id = Number(req.params.id);
-//   console.log("Deleting person with ID: ", id);
-//   persons = persons.filter((p) => p.id !== id);
-//   console.log("Person deleted with ID: ", id);
-//   res.status(204).end();
-// });
 app.delete("/api/persons/:id", (req, res) => {
-  Person.findByIdAndDelete(req.params.id).then((_) =>
-    res.status(204).end()
-  ).catch(error => {
-    console.log("Error occurred", error.message);
-    return res.status(400).send({ error: 'Mal-formatted ID' });
-  });
+  Person.findByIdAndDelete(req.params.id)
+    .then((_) => res.status(204).end())
+    .catch((error) => {
+      console.log("Error occurred", error.message);
+      return res.status(400).send({ error: "Mal-formatted ID" });
+    });
 });
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
 
 app.use(unknownEndpoint);
 
