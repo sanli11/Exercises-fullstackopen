3d3359d26cdf1aa1ffb100830c8e3a7556c9f66a
SanLi <fate.sinz1@gmail.com>
2023-12-12 14:45:48 -0800

    uibuild

--- Part-3/phonebook-backend/index.js
+++ Part-3/phonebook-backend/index.js
@@ -184,6 +184,8 @@ const errorHandler = (err, req, res, next) => {
 
   if (err.name === "CastError") {
     return res.status(400).send({ error: "Mal-formatted ID" });
+  } else if (err.name === "ValidationError") {
+    return res.status(400).send({ error: err.message });
   }
 
   next(err);
