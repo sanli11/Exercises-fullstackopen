6b1d3cde25013fae3dfb2ad504e1ef47b5491be1
SanLi <fate.sinz1@gmail.com>
2023-12-26 13:21:48 -0800

    Part 4.5 Completed

--- Part-4/blog-list/utils/list_helper.js
+++ Part-4/blog-list/utils/list_helper.js
@@ -3,4 +3,16 @@ const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) =>{
+  let mostLiked = blogs.reduce((prev, curr) => (prev.likes > curr.likes) ? prev : curr);
+
+  let mostLikedBlog = {
+    title: mostLiked.title,
+    author: mostLiked.author,
+    likes: mostLiked.likes
+  };
+
+  return mostLikedBlog;
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
