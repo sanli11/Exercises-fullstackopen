0f3c4586f99d0ead6d72519f22213152333547ed
SanLi <fate.sinz1@gmail.com>
2023-12-26 11:54:49 -0800

    Part 4.4 completed

---
+++ Part-4/blog-list/tests/list_helper.test.js
@@ -0,0 +1,57 @@
+const listHelper = require("../utils/list_helper");
+
+test("dummy returns one", () => {
+  const blogs = [];
+
+  const result = listHelper.dummy(blogs);
+  expect(result).toBe(1);
+});
+
+describe("total likes", () => {
+  const emptyList = [];
+
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    }
+  ];
+
+  const listWithMultipleBlogs = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "Introducing Windows 11",
+      author: "Panos Panay",
+      url: "https://blogs.windows.com/windowsexperience/2021/06/24/introducing-windows-11",
+      likes: 7,
+      __v: 0
+    },
+  ];
+
+  test("of empty list is zero", () => {
+    const result = listHelper.totalLikes(emptyList);
+    expect(result).toBe(0);
+  });
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(5);
+  });
+
+  test("of a bigger list ic calculated right", () => {
+    const result = listHelper.totalLikes(listWithMultipleBlogs);
+    expect(result).toBe(12);
+  });
+});
